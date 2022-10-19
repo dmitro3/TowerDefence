29c8e0048068da679de8a7553f717e737e05ef63
alexaport <39511315+alexaport@users.noreply.github.com>
2022-10-19 11:11:42 +0530

    Final contracts updated

--- TowerDefence/Assets/Scripts/BlockChain/CovalentManager.cs
+++ TowerDefence/Assets/Scripts/BlockChain/CovalentManager.cs
@@ -26,50 +26,49 @@ public class CovalentManager : MonoBehaviour
         while (true)
         {
             BalanceFetchPreURL = "https://api.covalenthq.com/v1/" + chainID + "/address/";
-            if (!chainID.Equals("250"))
+
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(BalanceFetchPreURL + SingletonDataManager.userethAdd + BalanceFetchPostURL))
             {
-                using (UnityWebRequest webRequest = UnityWebRequest.Get(BalanceFetchPreURL + SingletonDataManager.userethAdd + BalanceFetchPostURL))
+                webRequest.timeout = 10;
+                yield return webRequest.SendWebRequest();
+                switch (webRequest.result)
                 {
-                    webRequest.timeout = 10;
-                    yield return webRequest.SendWebRequest();
-                    switch (webRequest.result)
-                    {
-                        case UnityWebRequest.Result.ConnectionError:
-                            break;
-                        case UnityWebRequest.Result.DataProcessingError:
-                            Debug.LogError("Error: " + webRequest.error);
-                            break;
-                        case UnityWebRequest.Result.ProtocolError:
-                            Debug.LogError("HTTP Error: " + webRequest.error);
-                            break;
-                        case UnityWebRequest.Result.Success:
-                            Debug.Log("Received: " + webRequest.downloadHandler.text);
+                    case UnityWebRequest.Result.ConnectionError:
+                        break;
+                    case UnityWebRequest.Result.DataProcessingError:
+                        Debug.LogError("Error: " + webRequest.error);
+                        break;
+                    case UnityWebRequest.Result.ProtocolError:
+                        Debug.LogError("HTTP Error: " + webRequest.error);
+                        break;
+                    case UnityWebRequest.Result.Success:
+                        Debug.Log("Received: " + webRequest.downloadHandler.text);
 
-                            JSONObject _data = new JSONObject(webRequest.downloadHandler.text);
+                        JSONObject _data = new JSONObject(webRequest.downloadHandler.text);
 
-                            if (_data.GetField("data").HasField("items"))
+                        if (_data.GetField("data").HasField("items"))
+                        {
+                            for (int i = 0; i < _data.GetField("data").GetField("items").list.Count; i++)
                             {
-                                for (int i = 0; i < _data.GetField("data").GetField("items").list.Count; i++)
+                                var _add = _data.GetField("data").GetField("items")[i].GetField("native_token").boolValue;
+                                var temp2 = _data.GetField("data").GetField("items")[i].GetField("contract_address").stringValue.ToLower();
+
+                                if (_add)
                                 {
-                                    var _add = _data.GetField("data").GetField("items")[i].GetField("native_token").boolValue;
-                                    var temp2 = _data.GetField("data").GetField("items")[i].GetField("contract_address").stringValue.ToLower();
+                                    usrMainBalance = Math.Round((double)UnitConversion.Convert.FromWei(BigInteger.Parse(_data.GetField("data").GetField("items")[i].GetField("balance").stringValue)), 4).ToString();
+                                }
 
-                                    if (_add)
-                                    {
-                                        usrMainBalance = Math.Round((double)UnitConversion.Convert.FromWei(BigInteger.Parse(_data.GetField("data").GetField("items")[i].GetField("balance").stringValue)), 4).ToString();
-                                    }
+                                if (MoralisManager.contractToken.ToLower().Equals(temp2))
+                                {
+                                    usrTokenBalance = Math.Round((double)UnitConversion.Convert.FromWei(BigInteger.Parse(_data.GetField("data").GetField("items")[i].GetField("balance").stringValue)), 4).ToString();
 
-                                    if (MoralisManager.contractToken.ToLower().Equals(temp2))
-                                    {
-                                        usrTokenBalance = Math.Round((double)UnitConversion.Convert.FromWei(BigInteger.Parse(_data.GetField("data").GetField("items")[i].GetField("balance").stringValue)), 4).ToString();
-
-                                    }
                                 }
                             }
-                            break;
-                    }
-
+                        }
+                        break;
                 }
+
+
             }
             yield return new WaitForSeconds(UnityEngine.Random.Range(11, 30));
         }
